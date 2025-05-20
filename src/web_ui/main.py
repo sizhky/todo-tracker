dce1a87e7ab263cc6b4cd5e18eef3e8768183fe4
yeshwanth <yeshwanth@nanonets.com>
2025-05-20 12:14:57 +0530

    Add HTML target to makefile and enhance web UI with QR code generation

--- src/web_ui/main.py
+++ src/web_ui/main.py
@@ -1,28 +1,64 @@
+from torch_snippets import makedir, P
 from fasthtml.common import *
-from todo_tracker.tasks.manager import TaskManager, Task
+from fasthtml.components import Zero_md
+from td.v2.cli.display import fetch
+from td.v2.cli.sector import _list_sectors
+from subprocess import run
+import qrcode
+
+zeromd_headers = [
+    Script(type="module", src="https://cdn.jsdelivr.net/npm/zero-md@3?register")
+]
+static_path = "static"
+app, rt = fast_app(hdrs=zeromd_headers, static_path=static_path, static_url="/static")
+
+opened = set("_")
 
 
-@patch
-def __ft__(self: Task):
-    info = f"{self.title} - {self.description}" if self.description else self.title
-    return Li(P(info))
+def render_local_md(text, css=""):
+    css_template = Template(Style(css), data_append=True)
+    md = f"```text\n{text}\n```"
+    return Zero_md(css_template, Script(md, type="text/markdown"))
 
 
-task_manager = TaskManager()
-
-app, rt = fast_app()
-
-
-def Tasks():
-    tasks = task_manager.list_tasks()
-    return Ul(
-        *[task for task in tasks],
+def render_sector(sector):
+    return Details(
+        Summary(H3(sector)),
+        render_local_md(fetch(sector=sector)),
+        open=True if sector in opened else False,
     )
+
+
+@rt("/{fname:path}.{ext:static}")
+async def get(fname: str, ext: str):
+    print(fname, ext)
+    current_dir = P(__file__).resolve().parent
+    file_path = current_dir / "public" / f"{fname}.{ext}"
+    return FileResponse(file_path)
 
 
 @rt("/")
 def get():
-    return Titled("Hello", Div("Tasks", Tasks()))
+    return Titled(
+        Div(A("Hello", href="/mobile"), style="text-align: center;"),
+        *[render_sector(sector) for sector in _list_sectors()],
+    )
 
 
-serve()
+@rt("/mobile")
+def get():
+    ip = run(
+        "ifconfig | grep 192", shell=True, capture_output=True, text=True
+    ).stdout.split()[1]
+    ip = f"http://{ip}:1234"
+    img = qrcode.make(ip)
+    makedir(static_path)
+    img.save(f"{static_path}/ip.png")
+    return Titled(
+        "Scan QR",
+        Div(Img(src="ip.png"), Div(ip), style="text-align: center;"),
+    )
+
+
+if __name__ == "__main__":
+    serve(port=1234)
